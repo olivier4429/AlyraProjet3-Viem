8d7ce1d45b459a4f79e1383a52cf751c906fa2a8
olivier4429 <obervas@yahoo.com>
2026-01-14 09:37:57 +0100

    Update chains configuration based on environment

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,11 +1,21 @@
 import { mainnet, sepolia, hardhat } from 'wagmi/chains'
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
+// Déterminer les chains selon l'environnement
+const isProduction = import.meta.env.VITE_APP_ENV === 'vercel';
+
+
+// Chains conditionnelles
+const chains = isProduction 
+  ? [sepolia] // ✅ Vercel : SEULEMENT Sepolia
+  : [hardhat, sepolia, mainnet]; // 🛠️ Dev : tous les réseaux
+
+
 
 export const config = getDefaultConfig({
   appName: 'Projet3 app',
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
-  chains: [hardhat,mainnet, sepolia],
+  chains: chains,
 });
 
 
