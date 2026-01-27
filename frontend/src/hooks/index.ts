3a5e92d2d9bfae00017242ed1d2e2e2dc712d424
olivier <obervas@yahoo.com>
2026-01-27 10:22:34 +0100

    retour arriere sur le code Generené par Claude

--- frontend/src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-export { useOwner } from './useOwner';
-export { useWorkflowStatus } from './useWorkflowStatus';
-export { useVoter } from './useVoter';
-export { useListVoters } from './useListVoters';
-export { useProposals } from './useProposals';
-export { useContractWrite } from './useContractWrite';
-export { useWinningProposal } from './useWinningProposal';
